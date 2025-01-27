8d57da7faaa351ab0f9cb35db0781247b04aa3bf
zwethu <zwethuraaung1507@gmail.com>
2025-01-28 00:26:06 +0700

    initial commit

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+public class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public void link(Node node) {
+		this.next = node;
+	}
+
+	public int getElement() {
+		return this.data;
+	}
+
+	public Node getNextNode() {
+		return this.next;
+	}
+}
